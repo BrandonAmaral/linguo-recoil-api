cfd3e6c7cd44ab6ebae4304d40817dbf7a63855b
Brandon <brandon.amaral9658@gmail.com>
2021-04-26 18:10:23 -0300

    test: ensure AddDeckController calls AddDeck with correct values

--- tests/presentation/controllers/add-deck.spec.ts
+++ tests/presentation/controllers/add-deck.spec.ts
@@ -1,13 +1,40 @@
 import { AddDeckController } from '@/presentation/controllers';
+import { AddDeckSpy } from '@/tests/presentation/mocks';
 
-const makeSut = (): AddDeckController => {
-  return new AddDeckController();
+import faker from 'faker';
+
+const mockRequest = (): any => ({
+  name: faker.random.words(),
+  isPublic: true,
+});
+
+type SutTypes = {
+  sut: AddDeckController;
+  addDeckSpy: AddDeckSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const addDeckSpy = new AddDeckSpy();
+  const sut = new AddDeckController(addDeckSpy);
+  return {
+    sut,
+    addDeckSpy,
+  };
 };
 
 describe('AddDeck Controller', () => {
   it('Should return 200 on success', async () => {
-    const sut = makeSut();
-    const response = await sut.handle('any_request');
+    const { sut } = makeSut();
+    const request = mockRequest();
+    const response = await sut.handle(request);
     expect(response.statusCode).toBe(200);
+  });
+
+  it('Should call AddDeck with correct values', async () => {
+    const { sut, addDeckSpy } = makeSut();
+    const addSpy = jest.spyOn(addDeckSpy, 'add');
+    const request = mockRequest();
+    await sut.handle(request);
+    expect(addSpy).toHaveBeenCalledWith(request);
   });
 });
